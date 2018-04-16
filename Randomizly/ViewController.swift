ee740a434c7f05b95b561c2f4cb84eeef56d344d
Joseph <joseph@getmimo.com>
2018-04-16 16:29:34 +0200

    End of Adding a button chapter

--- Randomizly/ViewController.swift
+++ Randomizly/ViewController.swift
@@ -4,9 +4,17 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var randomNumberLabel: UILabel!
   
+  var number = 10
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    let number = 10
-    self.randomNumberLabel.text = "\(number)"
+    self.newRandomNumber(self)
+    
+  }
+  
+  @IBAction func newRandomNumber(_ sender: Any) {
+    self.number = Int(arc4random_uniform(6)+1)
+    self.randomNumberLabel.text = "\(self.number)"
+    self.view.backgroundColor = UIColor.red
   }
 }
