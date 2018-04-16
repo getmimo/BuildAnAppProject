b0c3f0a91bf12db55c0ac36b3557798025a1df5b
Joseph <joseph@getmimo.com>
2018-04-16 16:09:47 +0200

    End of Adding a Label chapter

--- Randomizly/ViewController.swift
+++ Randomizly/ViewController.swift
@@ -1,17 +1,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    
-  }
-
-
+  
 }
-
