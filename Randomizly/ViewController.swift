98837fb97b082f2a9c620f4ce79f884db7144dcf
Joseph <joseph@getmimo.com>
2018-04-16 16:16:02 +0200

    End of Displaying things chapter

--- Randomizly/ViewController.swift
+++ Randomizly/ViewController.swift
@@ -2,9 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  @IBOutlet weak var randomNumberLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    let number = 10
+    self.randomNumberLabel.text = "\(number)"
   }
-  
 }
