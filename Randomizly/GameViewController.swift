99398cf50627ca5fe856a063b8453cabd98a55ba
Joseph <joseph@getmimo.com>
2018-04-16 17:02:16 +0200

    End of Starting again chapter

---
+++ Randomizly/GameViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+      
+    }
+
+}
--- Randomizly/ViewController.swift
+++ Randomizly/ViewController.swift
@@ -2,19 +2,5 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  @IBOutlet weak var randomNumberLabel: UILabel!
-  
-  var number = 10
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.newRandomNumber(self)
-    
-  }
-  
-  @IBAction func newRandomNumber(_ sender: Any) {
-    self.number = Int(arc4random_uniform(6)+1)
-    self.randomNumberLabel.text = "\(self.number)"
-    self.view.backgroundColor = UIColor.red
-  }
 }
+
