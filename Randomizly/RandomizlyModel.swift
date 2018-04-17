a8c3a9dfa18b3689baf1577cc348f20541e0e2cc
Joseph <joseph@getmimo.com>
2018-04-17 09:21:05 +0200

    End of Guessing correctly chapter

--- Randomizly/RandomizlyModel.swift
+++ Randomizly/RandomizlyModel.swift
@@ -1,17 +1,19 @@
 import Foundation
 
 class RandomizlyModel {
-  var number = 0
+  private var number = 0
+  private(set) var tries = 0
   
   init() {
     self.newNumber()
   }
   
   func newNumber() {
-    self.number = Int(arc4random_uniform(6) + 1)
+    self.number = Int(arc4random_uniform(6) + 1 )
   }
   
   func guess(_ guess: Int) -> Bool {
+    self.tries += 1
     return self.number == guess
   }
 }
